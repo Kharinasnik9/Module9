513938ef017d93883419930ee09abde06cd541e5
Nikita Kharin <harin-1990@yandex.ru>
2024-11-15 22:26:47 +0400

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Exception[] Exceptions = new Exception[] 
+            { 
+                new ArgumentNullException("Исключение ArgumentNullException"),
+                new ArithmeticException("Исключение ArithmeticException"),
+                new InvalidDataException("Исключение InvalidDataException"),
+                new InvalidProgramException("Исключение InvalidProgramException"),
+                new MyException("Собственное исключение MyException")
+            };
+
+            foreach (var Ex in Exceptions)
+            {
+                try
+                {
+                    throw Ex;
+                }
+                catch (MyException MyEx)
+                {
+                    Console.WriteLine(MyEx.Message);
+                }
+                catch (ArgumentNullException ANEx)
+                {
+                    Console.WriteLine(ANEx.Message);
+                }
+                catch (ArithmeticException AEx)
+                {
+                    Console.WriteLine(AEx.Message);
+                }
+                catch (InvalidDataException IDEx)
+                {
+                    Console.WriteLine(IDEx.Message);
+                }
+                catch (InvalidProgramException IPEx)
+                {
+                    Console.WriteLine(IPEx.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Finally выполнен");
+                }
+
+
+
+            }
         }
     }
 }
